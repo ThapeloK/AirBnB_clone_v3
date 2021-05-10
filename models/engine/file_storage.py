1e1e414d0e5d18a37cede8ff6dd75c9a32f4183e
ThapeloK <e.thapelo@yahoo.com>
2021-05-10 20:37:11 +0000

    Added storage improvement

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -59,7 +59,7 @@ class FileStorage:
             pass
 
     def delete(self, obj=None):
-        """delete obj from __objects if it’s inside"""
+        """delete obj from __objects if its inside"""
         if obj is not None:
             key = obj.__class__.__name__ + '.' + obj.id
             if key in self.__objects:
@@ -68,3 +68,15 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """ Method retrieves one object """
+        key = "{}.{}".format(cls, id)
+        if cls and id and key in self.all(cls):
+            return self.all(cls)[key]
+        else:
+            return None
+
+    def count(self, cls=None):
+        """ Method counts the number of objects """
+        return len(self.all(cls))
